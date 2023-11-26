28544a6ec2940ec86d09b3d8e823bf170de1669a
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-11-26 23:46:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
- if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+ function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
     return 366; // Leap year
   } else {
     return 365; // Non-leap year
   }
+}
 
 let name = prompt("daysOfAYear");
+console.log(name);
